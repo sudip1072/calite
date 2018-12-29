822ef1625a7735eed4581c0a2623edd3f0b8dd07
sudip <sudipzoom@rediffmail.com>
2018-12-29 13:38:33 +0530

    Load google Api and create google map component

--- src/components/rental/rental-listing/RentalListing.js
+++ src/components/rental/rental-listing/RentalListing.js
@@ -4,17 +4,20 @@ import {connect} from 'react-redux';
 import * as actions from 'actions';
 
 
+
+
+
+
+
+
+
+
  class RentalListing extends Component {
 
-
-
 componentWillMount(){
+  
 this.props.dispatch(actions.fetchRentals());
-
-
 }
-
-
   render() { 
     return (
         <section id='rentalListing'>
