856e9ae800ea4bcbce1224a7e86ee860b62d6b3e
sudip <sudipzoom@rediffmail.com>
2018-12-31 00:53:56 +0530

    Add Login and register page.get authentification

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,23 @@
 import axios from 'axios';
-import {FETCH_RENTALS_SUCCESS, FETCH_RENTAL_BY_ID_SUCCESS,FETCH_RENTAL_BY_ID_INIT } from './types';
+import authService from 'services/auth-service';
+import axiosService from 'services/axios-service';
+
+import {FETCH_RENTALS_SUCCESS, 
+    FETCH_RENTAL_BY_ID_SUCCESS,
+    FETCH_RENTAL_BY_ID_INIT,
+    LOGIN_SUCCESS,
+    LOGIN_FAILURE,
+    LOGOUT 
 
 
+
+} from './types';
+
+
+
+// RENTALS ATIONS ---------------------------
+
+const axiosInstance = axiosService.getInstance();
 
 
 const fetchRentalByIdInit = ()=>{
@@ -34,7 +50,7 @@ const fetchRentalByIdInit = ()=>{
 
 export const fetchRentals = ()=>{
     return dispatch =>{
-    axios.get('/api/v1/rentals')
+      axiosInstance.get('/rentals')
     .then(res =>  res.data )
     .then(rentals => dispatch(fetchRentalsSuccess(rentals))
     );
@@ -58,3 +74,61 @@ export const fetchRentalById =(rentalId)=>{
 
 }
 
+
+// AUTH ACTIONS ---------------------------
+
+
+
+export const register = (userData) => {
+    return axios.post('/api/v1/users/register', {...userData}).then(
+      res => res.data,
+      err => Promise.reject(err.response.data.errors)
+    )
+  }
+
+  const loginSuccess = () => {
+    return {
+        type: LOGIN_SUCCESS
+        
+      }
+  }
+
+  const loginFailure = (errors) => {
+    return {
+      type: LOGIN_FAILURE,
+      errors
+    }
+  }
+
+  export const checkAuthState = () => {
+    return dispatch => {
+      if (authService.isAuthenticated()) {
+        dispatch(loginSuccess());
+      }
+    }
+  }
+
+  export const login = (userData) => {
+    return dispatch => {
+        return axios.post('/api/v1/users/auth', {...userData})
+        .then(res => res.data)
+        .then(token => {
+            authService.saveToken(token);
+            dispatch(loginSuccess());
+            
+        })
+        .catch(({response}) => {
+            dispatch(loginFailure(response.data.errors));
+          })
+
+    }
+
+  }
+
+  export const logout = () => {
+    authService.invalidateUser();
+  
+    return {
+      type: LOGOUT
+    }
+  }
