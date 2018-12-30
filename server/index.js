e0b87404af90f1159bf382c4cf7756c5357d9560
sudip <sudipzoom@rediffmail.com>
2018-12-30 11:12:37 +0530

    Add register and login authorization

--- server/index.js
+++ server/index.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const config = require('./config/dev');
 const Rental = require('./models/rental');
 
-const rentalRoutes = require('./routes/rentals');
-
+const rentalRoutes = require('./routes/rentals'),
+       userRoutes = require('./routes/users');
 
 const FakeDb = require('./fake-db');
 
 mongoose.connect(config.DB_URI,{ useNewUrlParser: true }).then(()=>{
   const fakeDb = new FakeDb();
   fakeDb.seedDb();
-})
+});
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use('/api/v1/rentals', rentalRoutes);
+app.use('/api/v1/users', userRoutes);
 
 const PORT = process.env.PORT || 3001;
 
